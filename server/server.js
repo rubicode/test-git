382c40afb23651a7198b45aa9b9ff6a75435e08d
agusbandung <agusana1544@gmail.com>
2018-03-03 21:16:56 +0700

    reactjs

--- server/server.js
+++ server/server.js
@@ -36,8 +36,8 @@ app.post('/api/phonebooks', function(req, res){
       console.error(err)
       process.exit(1)
     }
-    var phonebooks = JSON.parse(data)
-    var phonebook = {
+    let phonebooks = JSON.parse(data)
+    let phonebook = {
       id: req.body.id,
       name: req.body.name,
       phone: req.body.phone
@@ -46,11 +46,71 @@ app.post('/api/phonebooks', function(req, res){
     fs.writeFile(PHONEBOOKS_FILE, JSON.stringify(phonebooks, null, 3), function(err){
       if(err){
         console.error(err)
+        process.exit(1)
       }
       res.json(phonebook)
     })
   })
 })
+
+ app.get("/api/phonebooks/:id", function(req, res) {
+  fs.readFile(PHONEBOOKS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+    } else {
+      var phonebooks = JSON.parse(data);
+      var id = req.params.id;
+      console.log(id);
+      for (var i = 0; i < phonebooks.length; i++) {
+        if (phonebooks[i].id === id) {
+          phonebooks.splice(i, 1);
+          break;
+        }
+      }
+      console.log(phonebooks);
+    }
+    fs.writeFile(PHONEBOOKS_FILE, JSON.stringify(phonebooks, null, 3), function(err) {
+      if (err) {
+        console.error(err);
+      } else {
+        res.json(phonebooks);
+      }
+    });
+  });
+});
+
+app.post("/api/phonebooks/:id", function(req, res) {
+  let id = req.params.id;
+  let name = req.body.name;
+  let phone = req.body.phone;
+  console.log('ID : '+id);
+  console.log('Ini parameter input name dan phone : '+req.body);
+  fs.readFile(PHONEBOOKS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+    } else {
+      var phonebooks = JSON.parse(data);
+      for (var i = 0; i < phonebooks.length; i++) {
+        if (phonebooks[i].id === id) {
+          phonebooks[i].name = name;
+          phonebooks[i].phone = phone;
+          break;
+        }
+      }
+      console.log(phonebooks);
+    }
+    fs.writeFile(PHONEBOOKS_FILE, JSON.stringify(phonebooks, null, 3), function(err) {
+      if (err) {
+        console.error(err);
+      } else {
+        res.json(phonebooks);
+      }
+    });
+  });
+});
+
+
+
 
 app.listen(app.get('port'), function(){
   console.log('server berjalan di port ' + app.get('port'))
